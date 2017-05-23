bdf6f97642255dd5136b1dff7262d879454021ce
Alex Wood <alex@nderwood.com>
2017-05-23 04:47:19 -0700

    remove cruft from initial attempt at parsing input

--- main.rb
+++ main.rb
@@ -1,42 +1,41 @@
-class Simulation
-	def count_fallables(column)
-		column.count(".") + (column.count(":") * 2)
-	end
+def count_fallables(column)
+	column.count(".") + (column.count(":") * 2)
+end
 
-	def store_dimensions(columns, rows) 
-		@columns = columns
-		@rows = rows
-	end
+def store_dimensions(columns, rows) 
+	@columns = columns
+	@rows = rows
+end
 
-	def create_board(columns, rows)
-		@board = Array.new(rows){Array.new(columns)}
-	end
+def create_board(columns, rows)
+	@board = Array.new(rows){Array.new(columns)}
+end
 
-	def populate_row(input, input_index)
-		input.split("").each_with_index do |char, char_index|
-			if char_index <= (@columns - 1)
-				@board[input_index-1][char_index] = char
-			end
+def populate_row(input, input_index)
+	input.split("").each_with_index do |char, char_index|
+		if char_index <= (@columns - 1)
+			@board[input_index-1][char_index] = char
 		end
-	end
-
-	def setup(args)
-		args.each_with_index do |line, line_index|
-			if line_index == 0
-				store_dimensions(line[0].to_i, line[2].to_i)
-				create_board(@columns, @rows)
-			end
-
-			if line_index > 0 && line_index <= (@rows)
-				populate_row(line, line_index)
-			end
-		end
-	end
-
-	def simulate(board)
-		transposed = board.transpose
 	end
 end
+
+def setup(args)
+	args.each_with_index do |line, line_index|
+		if line_index == 0
+			store_dimensions(line[0].to_i, line[2].to_i)
+			create_board(@columns, @rows)
+		end
+
+		if line_index > 0 && line_index <= (@rows)
+			populate_row(line, line_index)
+		end
+	end
+end
+
+def simulate(board)
+	transposed = board.transpose
+end
+
 
 
 setup(ARGF)
